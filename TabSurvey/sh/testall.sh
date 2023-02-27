09ff5fe80cd7811fa48992bfd33e353eca6a4fed
7568 <v.cafebabe@gmail.com>
2023-02-27 20:23:22 +0800

    a

--- TabSurvey/sh/testall.sh
+++ TabSurvey/sh/testall.sh
@@ -4,3 +4,4 @@ bash torch_test.sh>torch_test.log &
 bash tnsorflow_test.sh>tnsorflow_test.log &
 bash sklearn_test.sh>sklearn_test.log &
 bash gbdt_test.sh>gbdt_test.log &
+bash attention_3d_test.sh>attention_3d_test.log &
